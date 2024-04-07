7daa6796e0416df66559a0d53df93f664b5bf747
hnishi <nishigami2509@gmail.com>
2024-04-07 16:46:38 +0900

    Add tests

---
+++ tests/test_main.py
@@ -0,0 +1,11 @@
+import llm_json_craft
+
+
+# class TestMain:
+#     def test_hello_world(self):
+#         result = llm_json_craft.hello_world()
+#         assert result == "Hello, world!"
+
+def test_hello_world():
+    result = llm_json_craft.hello_world()
+    assert result == "Hello, world!"
